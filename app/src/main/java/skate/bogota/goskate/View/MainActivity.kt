59e4700a23394cddb943f8644a826b961e200b43
Geral <erisbg.513@gmail.com>
2019-07-26 11:05:02 -0500

    android-rx 7/26/2019

--- app/src/main/java/skate/bogota/goskate/View/MainActivity.kt
+++ app/src/main/java/skate/bogota/goskate/View/MainActivity.kt
@@ -1,7 +1,10 @@
 package skate.bogota.goskate.View
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 import skate.bogota.goskate.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
+
+    /**
+     *
+     */
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.home_btn ->{
+                val login = Intent(this, MainActivity::class.java)
+                startActivity(login)
+            }
+            R.id.map_btn ->{
+                val mapa = Intent(this, MapsGoSkate::class.java)
+                startActivity(mapa)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
 }
---
+++ app/src/main/java/skate/bogota/goskate/View/shopActivity.kt
@@ -0,0 +1,13 @@
+package skate.bogota.goskate.View
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import skate.bogota.goskate.R
+
+class shopActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop)
+    }
+}
