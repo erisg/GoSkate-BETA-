3d89e3ba6d00c2fcf2a949dc9eeabbafbf607727
Geral <erisbg.513@gmail.com>
2019-07-26 15:02:18 -0500

    viewPost 7/26/2019

--- app/src/main/java/skate/bogota/goskate/View/galeryActivity.kt
+++ app/src/main/java/skate/bogota/goskate/View/galeryActivity.kt
@@ -3,28 +3,26 @@ package skate.bogota.goskate.View
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.MenuItem
-import androidx.appcompat.app.ActionBar
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.bottom_nav
 import skate.bogota.goskate.R
 
-class MainActivity : AppCompatActivity() {
-
+class galeryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_galery)
 
         bottom_nav.setOnNavigationItemSelectedListener { item ->
+
             when (item.itemId) {
+
                 R.id.nav_map_btn -> {
                     val login = Intent(this, MapsGoSkate::class.java)
                     startActivity(login)
                 }
 
-                R.id.cam_btn -> {
-                    val login = Intent(this, galeryActivity::class.java)
+                R.id.nav_home_btn -> {
+                    val login = Intent(this, MainActivity::class.java)
                     startActivity(login)
                 }
 
@@ -36,7 +34,5 @@ class MainActivity : AppCompatActivity() {
 
             true
         }
-
     }
-
 }
